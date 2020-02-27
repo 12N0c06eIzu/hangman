54c56c8ac620a45ae76b433521ea8acc93dfca1e
Rikuto Hayashi <ritu6811@gmail.com>
2020-02-27 17:23:40 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,46 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "____________",
+              "|    |      ",
+              "|    0      ",
+              "|   /|\     ",
+              "|    /\     ",
+              "|           "  
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+
+    win = False
+
+
+    print("Welcome to Hangman")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong +=1
+        print(" ".join(board))
+        e = wrong + 1
+        print(" \n".join(stages[0:e]))
+        if "_" not in board:
+            print("you win")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け正解は{}.".format(word))
+
+word_list = ["cat", "dog", "train", "unk"]
+
+word_listindex = random.randint(0, len(word_list))
+hangman(word_list[word_listindex])
